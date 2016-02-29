7d18643d70192e91178b152213bc1229b2917680
Budzanowski <bbudz@wro-bbudz-ml-01.local>
2016-02-29 10:20:27 +0100

    Revert "Restore embedded videos that went missing after 1.5 upgrade"
    This reverts commit e9d8e0fc5ea72f4a0c75b651b9df21908c548e15.

--- templates/liveblog-single-entry.php
+++ templates/liveblog-single-entry.php
@@ -5,9 +5,7 @@
 		<span class="liveblog-meta-time"><a href="#liveblog-entry-<?php echo absint( $entry_id ); ?>"><span class="date"><?php echo esc_html( $entry_date ); ?></span><span class="time"><?php echo esc_html( $entry_time ); ?></span></a></span>
 	</header>
 	<div class="liveblog-entry-text" data-original-content="<?php echo esc_attr( $original_content ); ?>">
-		<?php
-			echo $content; /* escaped earlier with wp_kses_post in WPCOM_Liveblog_Entry::render_content() */
-		 ?>
+		<?php echo wp_kses_post( $content ); ?>
 	</div>
 <?php if ( $is_liveblog_editable ): ?>
 	<ul class="liveblog-entry-actions">
